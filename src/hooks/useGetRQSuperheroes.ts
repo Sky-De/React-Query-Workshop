f637407283ac3b5bf0267e62e04879f5b26d8be0
skyDe <sky_de1991@outlook.com>
2024-05-28 06:51:24 -0800

    onSuccess and onError callbacks added

--- src/hooks/useGetRQSuperheroes.ts
+++ src/hooks/useGetRQSuperheroes.ts
@@ -3,21 +3,28 @@ import { SuperHeroes } from "./useGetReqularSuperheroes";
 import axios from "axios";
 
 const fetchSuperHeroes = () => axios.get("http://localhost:1313/superheroes");
+const onSuccess = (data: { data: SuperHeroes }) =>
+  console.log("Perform side effect after data fetching", data.data);
+const onError = (error: Error) =>
+  console.log("Perform side effect after encountering error", error);
 
 export const useGetRQSuperheroes = () => {
   const { data, isLoading, isError, refetch, error } = useQuery<
     { data: SuperHeroes },
     Error
   >("super-heroes", fetchSuperHeroes, {
-    // default true
+    // // default true
     // refetchOnMount: true,
-    // default true
+    // // default true
     // refetchOnWindowFocus: "always",
-    // polling (refetchInterval) - default false
-    // while browser is not in focus it will stop
+    // // polling (refetchInterval) - default false
+    // // while browser is not in focus it will stop
     // refetchInterval: 1000,
-    // while browser is not in focus it will continue
+    // // while browser is not in focus it will continue
     // refetchIntervalInBackground: true,
+    // // Performs side effects base on res status
+    onSuccess,
+    onError,
   });
   const superheroes = data?.data || [];
   const fetchStatus = {
