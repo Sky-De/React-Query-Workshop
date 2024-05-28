996e6f9800bed74f40609ce51eac1185717f3bbc
skyDe <sky_de1991@outlook.com>
2024-05-28 06:27:12 -0800

    link of svg commented out in index.html - refetchOnMount / refetchOnWindowFocus added

--- src/hooks/useGetRQSuperheroes.ts
+++ src/hooks/useGetRQSuperheroes.ts
@@ -2,12 +2,15 @@ import { useQuery } from "react-query";
 import { SuperHeroes } from "./useGetReqularSuperheroes";
 import axios from "axios";
 
+const fetchSuperHeroes = () => axios.get("http://localhost:1313/superheroes");
+
 export const useGetRQSuperheroes = () => {
   const { data, isLoading, isError, refetch, error } = useQuery<
     { data: SuperHeroes },
     Error
-  >("super-heroes", () => {
-    return axios.get("http://localhost:1313/superheroes");
+  >("super-heroes", fetchSuperHeroes, {
+    refetchOnMount: true,
+    refetchOnWindowFocus: "always",
   });
   const superheroes = data?.data || [];
   const fetchStatus = {
