9d85142fff92a15be7246eef2581d38113477681
skyDe <sky_de1991@outlook.com>
2024-05-28 06:34:32 -0800

    refetchInterval/refetchIntervalInBackground added

--- src/hooks/useGetRQSuperheroes.ts
+++ src/hooks/useGetRQSuperheroes.ts
@@ -9,8 +9,15 @@ export const useGetRQSuperheroes = () => {
     { data: SuperHeroes },
     Error
   >("super-heroes", fetchSuperHeroes, {
-    refetchOnMount: true,
-    refetchOnWindowFocus: "always",
+    // default true
+    // refetchOnMount: true,
+    // default true
+    // refetchOnWindowFocus: "always",
+    // polling (refetchInterval) - default false
+    // while browser is not in focus it will stop
+    // refetchInterval: 1000,
+    // while browser is not in focus it will continue
+    // refetchIntervalInBackground: true,
   });
   const superheroes = data?.data || [];
   const fetchStatus = {
